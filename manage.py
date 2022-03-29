021c2b4ccf263c7a5805d54f5cf7b235abe250e8
Anna Makarudze <amakarudze@gmail.com>
2022-03-29 03:22:26 +0200

    Format manage.py with black

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-import dotenv 
+import dotenv
 
-    
+
 def main():
     dotenv.read_dotenv()
 
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'activity_generator.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "activity_generator.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -22,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
