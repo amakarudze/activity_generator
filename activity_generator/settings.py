25c012543b7737d0eaba7ddc9daf99ff20c21582
Anna Makarudze <amakarudze@gmail.com>
2022-03-29 17:42:38 +0200

    Remove env_path setting

--- activity_generator/settings.py
+++ activity_generator/settings.py
@@ -108,8 +108,7 @@ if os.environ.get("GITHUB_WORKFLOW"):
             "PORT": "5432",
         }
     }
-    env_path = '.env_test'
-
+    
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
