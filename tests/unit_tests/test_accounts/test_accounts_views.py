a62044d6b30e67a783536c749d92c68b9d5b7a5d
Anna Makarudze <amakarudze@gmail.com>
2022-04-04 22:04:47 +0200

    Add tests for authenticated users

--- tests/unit_tests/test_accounts/test_accounts_views.py
+++ tests/unit_tests/test_accounts/test_accounts_views.py
@@ -64,3 +64,28 @@ def test_create_token_missing_field(client, user, token_missing_field):
 def test_retrieve_user_unauthorized(client):
     response = client.get(ME_URL)
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_retrieve_profile_success(user, user_client):
+    response = user_client.get(ME_URL)
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data == {
+        "email": user.email,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+    }
+
+
+def test_post_me_not_allowed(user, user_client):
+    response = user_client.post(ME_URL, {})
+    assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+
+
+def test_update_user_profile(update_user, user, user_client):
+    payload = update_user
+    response = user_client.patch(ME_URL, payload)
+    user.refresh_from_db()
+
+    assert user.first_name == payload["first_name"]
+    assert user.last_name == payload["last_name"]
+    assert response.status_code == status.HTTP_200_OK
