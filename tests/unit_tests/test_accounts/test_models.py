f17b86917a37221836dff2891384e3096ec363b1
Anna Makarudze <amakarudze@gmail.com>
2022-04-04 23:26:35 +0200

    Add setup.cfg, pytest markers and other settings

--- tests/unit_tests/test_accounts/test_models.py
+++ tests/unit_tests/test_accounts/test_models.py
@@ -4,18 +4,22 @@ from django.forms import ValidationError
 import pytest
 
 
+@pytest.mark.unit
 def test_user_string_representation(test_user):
     assert str(test_user) == "user@test.com"
 
 
+@pytest.mark.unit
 def test_user_get_full_name(test_user):
     assert test_user.get_full_name() == "Test User"
 
 
+@pytest.mark.unit
 def test_user_get_short_name(test_user):
     assert test_user.get_short_name() == "Test"
 
 
+@pytest.mark.unit
 def test_create_user_object_fails_without_email(db):
     with pytest.raises(ValidationError):
         get_user_model().objects.create_user(None, "First", "Last", "Testpass125")
